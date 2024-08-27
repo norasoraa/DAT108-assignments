4faf63a5ae2f0afe9e1b74b3d121b92c8fd274cf
Nora Soraa <notjsoraa@icloud.com>
2024-08-27 13:16:24 +0200

    task 1a

---
+++ src/oblig1/task1/Oppg1a.java
@@ -0,0 +1,16 @@
+package oblig1.task1;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Oppg1a {
+
+  public static void main(String[] args) {
+
+    List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
+    Collections.sort(listen, (x1, x2) -> Integer.valueOf(x1) - Integer.valueOf(x2));
+    System.out.println(listen);
+  }
+
+}
