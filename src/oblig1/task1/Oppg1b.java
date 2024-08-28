3b27280c2d1bb67f48a41e2b517726e5ddd1689a
Nora Soraa <notjsoraa@icloud.com>
2024-08-28 13:30:38 +0200

    task 1b

---
+++ src/oblig1/task1/Oppg1b.java
@@ -0,0 +1,25 @@
+package oblig1.task1;
+
+import java.util.function.BiFunction;
+
+public class Oppg1b {
+
+  public static void main(String[] args) {
+
+    // Oppg i. Summen av 12 og 13
+    BiFunction<Integer, Integer, Integer> sum = (a, b) -> a + b;
+    System.out.println(beregn(12, 13, sum));
+
+    // Oppg ii. Den største av -5 og 3
+    BiFunction<Integer, Integer, Integer> greatest = (a, b) -> Integer.max(a, b);
+    System.out.println(beregn(-5, 3, greatest));
+
+    // Oppg iii. Avstanden (absoluttverdien av differansen) mellom 54 og 45
+    BiFunction<Integer, Integer, Integer> difference = (a, b) -> Math.abs(a - b);
+    System.out.println(beregn(54, 45, difference));
+  }
+
+  private static int beregn(int a, int b, BiFunction<Integer, Integer, Integer> function) {
+    return function.apply(a, b);
+  }
+}
